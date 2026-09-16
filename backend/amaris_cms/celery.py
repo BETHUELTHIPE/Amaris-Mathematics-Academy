831f00cc3bcbbaa598599f7ed0d4c675be05f731
Bethuel Moukangwe <BETHUELTHIPE@GMAIL.COM>
2026-09-16 19:09:36 +0200

    reliability: register Celery dead-letter hooks

--- backend/amaris_cms/celery.py
+++ backend/amaris_cms/celery.py
@@ -7,3 +7,6 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "amaris_cms.settings")
 app = Celery("amaris_cms")
 app.config_from_object("django.conf:settings", namespace="CELERY")
 app.autodiscover_tasks()
+
+# Register reliability hooks after Celery has loaded Django settings.
+from amaris_cms import celery_signals as _celery_signals  # noqa: E402,F401
