7a8ffd1a8974199dc2ecdacdc889bd2536762b15
Bethuel Moukangwe <BETHUELTHIPE@GMAIL.COM>
2026-09-24 16:56:38 +0200

    fix: normalize custom video migration UUID default

--- backend/content/migrations/0008_custom_video_request.py
+++ backend/content/migrations/0008_custom_video_request.py
@@ -1,4 +1,5 @@
 import decimal
+import uuid
 
 import content.models
 from django.db import migrations, models
@@ -83,7 +84,7 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name="CustomVideoRequest",
             fields=[
-                ("id", models.UUIDField(default=content.models.uuid.uuid4, editable=False, primary_key=True, serialize=False)),
+                ("id", models.UUIDField(default=uuid.uuid4, editable=False, primary_key=True, serialize=False)),
                 ("created_at", models.DateTimeField(auto_now_add=True)),
                 ("updated_at", models.DateTimeField(auto_now=True)),
                 ("reference", models.CharField(max_length=100, unique=True)),
